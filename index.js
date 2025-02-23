4583e5a68e93cb189e9a798337b0ff15709c84d2
Blue_sky303 <blue_sky303@qq.com>
2025-02-23 18:11:17 +0800

    图片放大

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-var sidebarButton = document.querySelectorAll('.sidebar-button');
+var loginButton = document.querySelectorAll(".login-links a, .login-links nav a, .nav-links a");
+var sidebarButton = document.getElementsByClassName("sidebar-button");
+var serviceCategoriesButton = document.querySelectorAll(".service-categories span");
+var serviceButton = document.querySelectorAll(".service-list ul li");
 
-for (var i = 0; i < sidebarButton.length; i++) {
-    sidebarButton[i].onclick = function() {
-        zoom(this, 2, 1);
+for (let button of loginButton) {
+    button.onclick = function() {
+        /*点击跳转*/
     };
 }
+
+for (let button of sidebarButton) {
+    button.onmouseover = function() {
+        zoom(this, 2, 1);
+    };
+    button.onmouseout = function() {
+        zoom(this, 1, 0);
+    };
+    button.onclick = function() {
+        /*点击跳转*/
+    };
+}
+
+for (let button of serviceCategoriesButton) {
+    button.onclick = function() {
+        /*点击跳转*/
+    };
+}
+
+for (let button of serviceButton) {
+    button.onclick = function() {
+        /*点击跳转*/
+    };
+}
+
 
 function zoom(thing, scale, z) {
     thing.style.transform = `scale(${scale})`;
