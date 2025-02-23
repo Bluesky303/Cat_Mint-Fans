a6afed9871e160647a27106d5a7fa4b2a9496ce7
Blue_sky303 <blue_sky303@qq.com>
2025-02-24 00:04:25 +0800

    hero-section动画

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ var sidebarButton = document.getElementsByClassName("sidebar-button");
 var serviceCategoriesButton = document.querySelectorAll(".service-categories span");
 var serviceButton = document.querySelectorAll(".service-list ul li");
 var sliders = document.getElementsByClassName("slider-img");
+var heroSection = document.getElementsByClassName("hero-section")[0];
+
+heroSection.addEventListener("animationend", function() {
+    var done=setInterval('show()',60);
+});
+let textnum = 0;
+function show() {
+    var h1 = "欢迎使用现代化地府一体化政务服务平台";
+    var p = "一网通办，全流程一体化的解决您的问题";
+    var text = h1 + p;
+    document.querySelectorAll(".hero-section h1")[0].innerHTML = text.slice(0, textnum > h1.length ? h1.length : textnum) + "|";
+    if(textnum > h1.length) {
+        document.querySelectorAll(".hero-section h1")[0].innerHTML = h1;
+    }
+    document.querySelectorAll(".hero-section p")[0].innerHTML = textnum > h1.length ? text.slice(h1.length, textnum) + "|": "";
+    textnum++;
+    if(textnum + 1 >= h1.length + p.length + 1) {
+        clearInterval("done");
+        document.querySelectorAll(".hero-section p")[0].innerHTML = p;
+    }
+}
 
 for (let button of loginButton) {
     button.onclick = function() {
