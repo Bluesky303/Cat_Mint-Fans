a5e2b8e7c599572631678453a06add04f6b93ea1
Blue_sky303 <blue_sky303@qq.com>
2025-02-24 10:03:45 +0800

    模块淡入

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+var heroSection = document.getElementsByClassName("hero-section")[0];
+
+var sliders = document.getElementsByClassName("slider-img");
+
+
+
 var loginButton = document.querySelectorAll(".login-links a, .login-links nav a, .nav-links a");
 var sidebarButton = document.getElementsByClassName("sidebar-button");
 var serviceCategoriesButton = document.querySelectorAll(".service-categories span");
 var serviceButton = document.querySelectorAll(".service-list ul li");
-var sliders = document.getElementsByClassName("slider-img");
-var heroSection = document.getElementsByClassName("hero-section")[0];
 
+
+/*打字函数*/
 heroSection.addEventListener("animationend", function() {
     var done=setInterval('show()',60);
 });
@@ -25,6 +31,48 @@ function show() {
     }
 }
 
+/*模块出现*/
+function showModule(mod) {
+    var mod = document.getElementsByClassName(mod)[0];
+    var moduleShow = setInterval(function() {
+        var y = mod.getBoundingClientRect().y;
+        if (y < window.innerHeight) {
+            mod.id = ""
+            setTimeout(() => { mod.id = "v-show";}, 0);
+            clearInterval(moduleShow);
+        }
+    }, 10);
+}
+
+showModule("efficient-service-section");
+showModule("news-column-section");
+showModule("information-disclosure-section");
+showModule("feature-section");
+showModule("service-statistics-section");
+
+/*轮播图*/
+for (let slider of sliders) {
+    slider.addEventListener("mouseover", function() {
+        slider.addEventListener("mousemove", function(event) {
+            var xoffset = (event.offsetX *2 - slider.width) / slider.width;
+            var yoffset = (event.offsetY *2 - slider.height) / slider.height;
+            slider.style.transform = `scale(1.2) rotate3d(${Math.atan(yoffset)}, ${Math.atan(-xoffset)}, 0, 0.4rad)`;
+            slider.style.boxShadow = `${-xoffset*10}px ${-yoffset*10}px 10px white`;
+        });
+    });
+    slider.addEventListener("mouseout", function() {
+        slider.style.transform = "scale(1)";
+        slider.style.boxShadow = "10px 10px 10px white";
+    });
+}
+
+/*缩放函数*/
+function zoom(thing, scale, z) {
+    thing.style.transform = `scale(${scale})`;
+    thing.style.zIndex = z;
+}
+
+/*按钮 */
 for (let button of loginButton) {
     button.onclick = function() {
         /*点击跳转*/
@@ -48,34 +96,4 @@ for (let button of serviceCategoriesButton) {
         /*点击跳转*/
     };
 }
-
-for (let slider of sliders) {
-    slider.addEventListener("mouseover", function() {
-        slider.addEventListener("mousemove", function(event) {
-            var xoffset = (event.offsetX *2 - slider.width) / slider.width;
-            var yoffset = (event.offsetY *2 - slider.height) / slider.height;
-            console.log(Math.atan(yoffset)* 180, Math.atan(-xoffset)* 180);
-            slider.style.transform = `scale(1.2) rotate3d(${Math.atan(yoffset)}, ${Math.atan(-xoffset)}, 0, 0.4rad)`;
-            slider.style.boxShadow = `${-xoffset*10}px ${-yoffset*10}px 10px white`;
-        });
-    });
-    slider.addEventListener("mouseout", function() {
-        slider.style.transform = "scale(1)";
-        slider.style.boxShadow = "10px 10px 10px white";
-    });
-}
-
-
-function zoom(thing, scale, z) {
-    thing.style.transform = `scale(${scale})`;
-    thing.style.zIndex = z;
-}
-
-
-
-
-
-
-
-
 
