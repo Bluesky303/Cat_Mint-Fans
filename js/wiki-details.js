88c078ce0f1f6b33d12e38db242299f8f505c00b
knvbepr <3366039503@qq.com>
2025-04-23 19:25:07 +0800

    添加Wiki跳转，修复选项卡点击后footer显示错位的问题

--- js/wiki-details.js
+++ js/wiki-details.js
@@ -2,22 +2,35 @@ $(document).ready(function () {
     $("header").load("./framework/header.html");
     $("aside").load("./framework/sidebar.html");
     $("footer").load("./framework/footer.html");
+    // 动态调整内容区域高度
+    function adjustContentHeight() {
+        var windowHeight = $(window).height();
+        var headerHeight = $('header').outerHeight();
+        var sidebarHeight = $('aside').outerHeight();
+        var footerHeight = $('footer').outerHeight();
+        var contentHeight = windowHeight - headerHeight - footerHeight;
+
+        $('.tab-content').css('min-height', contentHeight + 'px');
+    }
+
+    adjustContentHeight();
+    $(window).resize(adjustContentHeight); // 当窗口大小改变时调整内容高度
 
     // 选项卡切换功能
-    $('.tablinks').on('click', function() {
+    $('.tablinks').on('click', function () {
         // 移除所有按钮的高亮颜色
         $('.tablinks').css('background-color', '');
-        $('.tablinks').css('color','white')
+        $('.tablinks').css('color', 'white')
         // 设置当前按钮的颜色
         $(this).css('background-color', '#111');
-        $(this).css('color','red')
+        $(this).css('color', 'red')
         // 获取所点击按钮的自定义属性
         var tabId = $(this).data('tab'); // 使用data-tab属性获取相应的内容ID
-        
+
         // 隐藏所有内容
         $('#0,#1, #2, #3, #4').hide();
-        
+
         // 显示对应内容
-        $('#' + tabId).show(); 
+        $('#' + tabId).show();
     });
 });
